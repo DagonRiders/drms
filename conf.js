f1a9b6022fce92c2852ff05e45261ee3ab954cc2
fallax <fallaburr@gmail.com>
2017-12-24 22:08:00 +0000

    Updated config file

--- conf.js
+++ conf.js
@@ -1,5 +1,5 @@
 var server = {
-	"address": "80.229.172.125:7379",
-	"username": "drmsweb",
-	"password": "06ded64a4a4b8051d350"
+	"address": "SERVER ADDRESS:SERVER PORT",
+	"username": "USERNAME GOES HERE",
+	"password": "PASSWORD GOES HERE"
 };
